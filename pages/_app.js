a3e965c6559148648f31290d640b47bfe32a7c34
facobackup <gustavomicaelbarbosa@gmail.com>
2021-12-20 21:32:26 -0300

    Ajustes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import '../styles/globals.css'
-import {Fabric} from "@f-ui/core";
+import {Button, Fabric, ToolTip} from "@f-ui/core";
 import styles from '../styles/Home.module.css'
+import {useState} from "react";
+
 function MyApp({Component, pageProps}) {
+    const [dark, setDark] = useState(true)
     return (
-        <Fabric theme={'light'} className={styles.wrapper} >
+        <Fabric theme={dark ? 'dark' : "light"} className={styles.wrapper}>
             <Component {...pageProps} />
+            <Button className={styles.button} onClick={() => setDark(!dark)}>
+                {dark ?
+                    <span className="material-icons-round">brightness_3</span>
+                    :
+                    <span className="material-icons-round">brightness_high</span>}
+                <ToolTip content={'Theme'} justify={'start'} align={'middle'}/>
+            </Button>
         </Fabric>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,8 @@ export default function Home() {
                         </Button>
                     </div>
                 ))}
+
+
             </div>
         </div>
     )
